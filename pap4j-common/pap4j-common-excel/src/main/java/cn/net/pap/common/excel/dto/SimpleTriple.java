8f066325a4404748f1c58c57b900b434e4218d71
alexgaoyh <alexgaoyh@sina.com>
2024-04-16 11:42:09 +0800

    add: triple 2 file method.

--- pap4j-common/pap4j-common-excel/src/main/java/cn/net/pap/common/excel/dto/SimpleTriple.java
+++ pap4j-common/pap4j-common-excel/src/main/java/cn/net/pap/common/excel/dto/SimpleTriple.java
@@ -1,6 +1,7 @@
 package cn.net.pap.common.excel.dto;
 
-import java.io.Serializable;
+import java.io.*;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -26,6 +27,9 @@ public class SimpleTriple<T1, T2, T3> implements Serializable {
      * 宾语
      */
     private T3 o;
+
+    public SimpleTriple() {
+    }
 
     public SimpleTriple(T1 s, T2 p, T3 o) {
         this.s = s;
@@ -78,6 +82,69 @@ public class SimpleTriple<T1, T2, T3> implements Serializable {
         sb.append(", o=").append(o);
         sb.append('}');
         return sb.toString();
+    }
+
+    /**
+     * 转换为三元组格式：  <s> <p> <o> .
+     * @return
+     */
+    public String toTriple() {
+        final StringBuffer sb = new StringBuffer("");
+        sb.append("<").append(convertSpecialCharsToUnicode(getS())).append("> ");
+        sb.append("<").append(convertSpecialCharsToUnicode(getP())).append("> ");
+        sb.append("<").append(convertSpecialCharsToUnicode(getO())).append("> ");
+        sb.append('.');
+        return sb.toString();
+    }
+
+    /**
+     * 特殊字符转换，比如空格
+     *
+     * @param input
+     * @return
+     */
+    public static String convertSpecialCharsToUnicode(Object input) {
+        if (input == null) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.toString().length(); i++) {
+            char c = input.toString().charAt(i);
+            switch (c) {
+                case ' ':
+                    sb.append("\\u0020"); // 空格
+                    break;
+                case '\n':
+                    sb.append("\\u000A"); // 换行符
+                    break;
+                case '\t':
+                    sb.append("\\u0009"); // 制表符
+                    break;
+                default:
+                    sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 写入文件，按行的三元组信息： <s> <p> <o> .
+     *
+     * @param fileNameAbsolutePath
+     * @param simpleTripleList
+     * @throws Exception
+     */
+    public void write2TripleFile(String fileNameAbsolutePath, List<SimpleTriple<T1, T2, T3>> simpleTripleList) throws Exception {
+        FileOutputStream fos = new FileOutputStream(new File(fileNameAbsolutePath));
+        OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        for (SimpleTriple simpleTriple : simpleTripleList) {
+            bw.write(simpleTriple.toTriple() + "\t\n");
+        }
+        bw.close();
+        osw.close();
+        fos.close();
     }
 
 }
