ee4c400d0364f6b992e739ca24b1321324b1d0c8
alexgaoyh <alexgaoyh@sina.com>
2024-05-22 14:52:35 +0800

    add: neo4j 根据 graphId 进行查询.

--- pap4j-boot3-starters/pap4j-boot3-starters-neo4j/src/main/java/cn/net/pap/neo4j/repository/HLMRepository.java
+++ pap4j-boot3-starters/pap4j-boot3-starters-neo4j/src/main/java/cn/net/pap/neo4j/repository/HLMRepository.java
@@ -2,10 +2,20 @@ package cn.net.pap.neo4j.repository;
 
 import cn.net.pap.neo4j.entity.HLMEntity;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 
 import java.util.List;
 
 public interface HLMRepository extends Neo4jRepository<HLMEntity, String> {
 
     public List<HLMEntity> findByName(String name);
+
+    /**
+     * 根据ID 匹配数据，这里的 ID 是 neo4j里默认会创建的一个Long型自增ID
+     * @param id
+     * @return
+     */
+    @Query("MATCH (n:HLM) WHERE id(n)={id} RETURN n")
+    public HLMEntity getById(Long id);
+
 }
--- pap4j-boot3-starters/pap4j-boot3-starters-neo4j/src/test/java/cn/net/pap/neo4j/HLMEntityTest.java
+++ pap4j-boot3-starters/pap4j-boot3-starters-neo4j/src/test/java/cn/net/pap/neo4j/HLMEntityTest.java
@@ -2,19 +2,22 @@ package cn.net.pap.neo4j;
 
 import cn.net.pap.neo4j.dto.HLMNodeWithTypeDTO;
 import cn.net.pap.neo4j.entity.HLMEntity;
+import cn.net.pap.neo4j.entity.HLMRelationshipEntity;
 import cn.net.pap.neo4j.repository.HLMRelationshipRepository;
 import cn.net.pap.neo4j.repository.HLMRepository;
 import cn.net.pap.neo4j.util.kg.HLMEntity2KGConvert;
 import cn.net.pap.neo4j.util.kg.PathValue2KGConvert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.neo4j.driver.internal.InternalNode;
 import org.neo4j.driver.internal.value.PathValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.neo4j.core.Neo4jClient;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {Neo4jApplication.class})
@@ -25,6 +28,9 @@ public class HLMEntityTest {
 
     @Autowired
     private HLMRelationshipRepository hlmRelationshipRepository;
+
+    @Autowired
+    private Neo4jClient neo4jClient;
 
     @Test
     public void findByName() {
@@ -47,4 +53,43 @@ public class HLMEntityTest {
         Map<String, Object> kgGraph = PathValue2KGConvert.convertToKnowledgeGraph(shortestPath);
         System.out.println(kgGraph);
     }
+
+    @Test
+    public void getById() {
+        HLMEntity byId = hlmRepository.getById(157L);
+        System.out.println(byId);
+    }
+
+    @Test
+    public void queryDTO(){
+        String cypherQuery = "MATCH (h:HLM)-[r]->(m:HLM) WHERE id(h)="+157+" RETURN h.name AS name, COLLECT({type: r.type, endNode: m}) AS relationships";
+        List<HLMEntity> results = neo4jClient.query(cypherQuery)
+                .fetchAs(HLMEntity.class)
+                .mappedBy((typeSystem, record) -> {
+                    HLMEntity dto = new HLMEntity();
+                    dto.setName(record.get("name").asString());
+
+                    Set<HLMRelationshipEntity> relationshipEntityList = new HashSet<>();
+                    List<Object> relationships = record.get("relationships").asList();
+                    if(relationships != null && relationships.size() > 0) {
+                        for(Object object : relationships) {
+                            HLMRelationshipEntity hlmRelationshipEntity = new HLMRelationshipEntity();
+                            Object typeObj = ((Map) object).get("type");
+                            hlmRelationshipEntity.setType(typeObj.toString());
+                            Object endNodeObj = ((Map) object).get("endNode");
+                            if(endNodeObj instanceof InternalNode) {
+                                InternalNode internalNode = (InternalNode)endNodeObj;
+                                HLMEntity hlmEntity = new HLMEntity();
+                                hlmEntity.setName(internalNode.asMap().get("name").toString());
+                                hlmRelationshipEntity.setEndNode(hlmEntity);
+                            }
+                            relationshipEntityList.add(hlmRelationshipEntity);
+                        }
+                    }
+                    dto.setRelationships(relationshipEntityList);
+                    return dto;
+                }).all().stream().collect(Collectors.toList());
+        System.out.println(results);
+    }
+
 }
