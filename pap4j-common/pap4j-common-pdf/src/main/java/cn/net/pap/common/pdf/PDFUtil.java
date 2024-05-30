71e1b5fd18b6731feb54c012d28ab485da2e741e
alexgaoyh <alexgaoyh@sina.com>
2024-05-30 14:08:22 +0800

    add: pdfbox 添加数字签名优化,签名验证成功.

--- pap4j-common/pap4j-common-pdf/src/main/java/cn/net/pap/common/pdf/PDFUtil.java
+++ pap4j-common/pap4j-common-pdf/src/main/java/cn/net/pap/common/pdf/PDFUtil.java
@@ -2,25 +2,30 @@ package cn.net.pap.common.pdf;
 
 import cn.net.pap.common.pdf.sign.SignatureInterfaceImpl;
 import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.io.IOUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
 import org.apache.pdfbox.pdmodel.encryption.StandardProtectionPolicy;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.apache.pdfbox.pdmodel.interactive.digitalsignature.ExternalSigningSupport;
 import org.apache.pdfbox.pdmodel.interactive.digitalsignature.PDSignature;
 import org.apache.pdfbox.pdmodel.interactive.digitalsignature.SignatureOptions;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.security.KeyStore;
 import java.security.PrivateKey;
+import java.security.cert.Certificate;
 import java.util.Calendar;
 
 public class PDFUtil {
 
     /**
      * 添加印章
+     *
      * @param pdfFilePath
      * @param imageFilePath
      * @param outputFilePath
@@ -41,9 +46,10 @@ public class PDFUtil {
 
     /**
      * 添加数字签名
+     *
      * @param pdfFilePath
-     * @param keystorePath
-     * @param keystorePassword
+     * @param keystorePath     *.p12   可以申请SSL证书(*.key, .crt)，然后使用openssl 命令转换为 p12 证书
+     * @param keystorePassword 密码
      * @param outputFilePath
      * @throws Exception
      */
@@ -53,25 +59,34 @@ public class PDFUtil {
             try (FileInputStream fis = new FileInputStream(keystorePath)) {
                 keystore.load(fis, keystorePassword.toCharArray());
             }
-            PrivateKey privateKey = (PrivateKey) keystore.getKey("alias", keystorePassword.toCharArray());
+            String alias = keystore.aliases().nextElement();
+            PrivateKey privateKey = (PrivateKey) keystore.getKey(alias, keystorePassword.toCharArray());
+            Certificate[] certificateChain = keystore.getCertificateChain(alias);
 
             PDSignature signature = new PDSignature();
             signature.setFilter(PDSignature.FILTER_ADOBE_PPKLITE);
             signature.setSubFilter(PDSignature.SUBFILTER_ADBE_PKCS7_DETACHED);
-            signature.setName("alexgaoyh name");
-            signature.setLocation("alexgaoyh location");
-            signature.setReason("alexgaoyh reason");
+            signature.setName("name");
+            signature.setLocation("location");
+            signature.setReason("reason");
             signature.setSignDate(Calendar.getInstance());
 
             SignatureOptions signatureOptions = new SignatureOptions();
-            document.addSignature(signature, new SignatureInterfaceImpl(privateKey), signatureOptions);
+            signatureOptions.setPreferredSignatureSize(SignatureOptions.DEFAULT_SIGNATURE_SIZE);
+            document.addSignature(signature, new SignatureInterfaceImpl(privateKey, certificateChain), signatureOptions);
 
-            document.save(outputFilePath);
+            try (FileOutputStream fos = new FileOutputStream(outputFilePath);) {
+                ExternalSigningSupport externalSigning = document.saveIncrementalForExternalSigning(fos);
+                byte[] content = IOUtils.toByteArray(externalSigning.getContent());
+                byte[] signedContent = SignatureInterfaceImpl.signContent(content, privateKey, certificateChain);
+                externalSigning.setSignature(signedContent);
+            }
         }
     }
 
     /**
      * 添加禁止编辑(密码)
+     *
      * @param pdfFilePath
      * @param ownerPassword
      * @param userPassword
--- pap4j-common/pap4j-common-pdf/src/main/java/cn/net/pap/common/pdf/sign/SignatureInterfaceImpl.java
+++ pap4j-common/pap4j-common-pdf/src/main/java/cn/net/pap/common/pdf/sign/SignatureInterfaceImpl.java
@@ -1,37 +1,56 @@
 package cn.net.pap.common.pdf.sign;
 
+import org.apache.pdfbox.io.IOUtils;
 import org.apache.pdfbox.pdmodel.interactive.digitalsignature.SignatureInterface;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.cert.jcajce.JcaCertStore;
+import org.bouncycastle.cms.CMSProcessableByteArray;
+import org.bouncycastle.cms.CMSSignedData;
+import org.bouncycastle.cms.CMSSignedDataGenerator;
+import org.bouncycastle.cms.jcajce.JcaSignerInfoGeneratorBuilder;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder;
+import org.bouncycastle.util.Store;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.PrivateKey;
-import java.security.Security;
-import java.security.Signature;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
 
 public class SignatureInterfaceImpl implements SignatureInterface {
-    private PrivateKey privateKey;
+    private final PrivateKey privateKey;
+    private final Certificate[] certificateChain;
 
-    public SignatureInterfaceImpl(PrivateKey privateKey) {
+    public SignatureInterfaceImpl(PrivateKey privateKey, Certificate[] certificateChain) {
         this.privateKey = privateKey;
+        this.certificateChain = certificateChain;
     }
 
     @Override
     public byte[] sign(InputStream content) throws IOException {
         try {
-            //使用Bouncycastle进行数字签名
-            Security.addProvider(new BouncyCastleProvider());
-            Signature signature = Signature.getInstance("SHA256withRSA", "BC");
-            signature.initSign(privateKey);
-            byte[] buffer = new byte[8192];
-            int length = 0;
-            while ((length = content.read(buffer)) != -1) {
-                signature.update(buffer, 0, length);
-            }
-            return signature.sign();
+            byte[] contentBytes = IOUtils.toByteArray(content);
+            return signContent(contentBytes, privateKey, certificateChain);
         } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+            throw new IOException(e);
         }
+    }
+
+    public static byte[] signContent(byte[] content, PrivateKey privateKey, Certificate[] certificateChain) throws Exception {
+        // 用于生成PKCS#7签名的代码，这里仅为示例，具体实现可能需要使用BouncyCastle或其他库
+        CMSSignedDataGenerator generator = new CMSSignedDataGenerator();
+        ContentSigner signer = new JcaContentSignerBuilder("SHA256WithRSA").build(privateKey);
+        generator.addSignerInfoGenerator(
+                new JcaSignerInfoGeneratorBuilder(
+                        new JcaDigestCalculatorProviderBuilder().build()
+                ).build(signer, (X509Certificate) certificateChain[0])
+        );
+        Store<?> certStore = new JcaCertStore(Arrays.asList(certificateChain));
+        generator.addCertificates(certStore);
+        CMSProcessableByteArray contentBytes = new CMSProcessableByteArray(content);
+        CMSSignedData signedData = generator.generate(contentBytes, false);
+        return signedData.getEncoded();
     }
 }
