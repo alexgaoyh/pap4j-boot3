25e7a8a7c18d5bcb0a460693c6420ee240dcd6e0
alexgaoyh <alexgaoyh@sina.com>
2024-06-11 10:13:07 +0800

    add: Deeplearning4jUtilss.

---
+++ pap4j-common/pap4j-common-deeplearning4j/src/main/java/cn/net/pap/common/deeplearning4j/Deeplearning4jUtilss.java
@@ -0,0 +1,13 @@
+package cn.net.pap.common.deeplearning4j;
+
+public class Deeplearning4jUtilss {
+
+    /**
+     * 获得引用的 deeplearning4j version
+     * @return
+     */
+    public static String getVersion() {
+        return "1.0.0-M2.1";
+    }
+
+}
