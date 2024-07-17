17de03cd696e1c60a2b9495266f86984199d339b
alexgaoyh <alexgaoyh@sina.com>
2024-07-17 14:01:52 +0800

    add: 层级优化.

--- pap4j-common/pap4j-common-datastructure/src/main/java/cn/net/pap/common/datastructure/catalog/CatalogUtil.java
+++ pap4j-common/pap4j-common-datastructure/src/main/java/cn/net/pap/common/datastructure/catalog/CatalogUtil.java
@@ -51,6 +51,30 @@ public class CatalogUtil {
         return null;
     }
 
+    /**
+     * 此方法相对于 findParent，区别在于允许层级不连续，允许'目录一'下面直接跟着'目录三'。
+     * @param map
+     * @param dto
+     * @return
+     */
+    private static CatalogTreeDTO findParent2(Map<String, CatalogTreeDTO> map, CatalogDTO dto) {
+        CatalogTreeDTO returnDTO = null;
+        int currentLevel = TYPE_LEVEL.get(dto.getType()) - 1;
+
+        while (currentLevel > 0) {
+            String parentType = getTypeByLevel(currentLevel);
+            CatalogTreeDTO potentialParent = map.get(parentType);
+            returnDTO = potentialParent;
+            if(returnDTO != null) {
+                break;
+            } else {
+                currentLevel--;
+            }
+        }
+
+        return returnDTO;
+    }
+
 
     private static String getTypeByLevel(int level) {
         for (Map.Entry<String, Integer> entry : TYPE_LEVEL.entrySet()) {
