b0d22c773a671098e8c3fe45dd93fa549bd52ef7
alexgaoyh <alexgaoyh@sina.com>
2024-08-16 13:59:37 +0800

    add: FST test using billion data.

--- pap4j-common/pap4j-common-datastructure/src/test/java/cn/net/pap/common/datastructure/catalog/FSTUtilTest.java
+++ pap4j-common/pap4j-common-datastructure/src/test/java/cn/net/pap/common/datastructure/catalog/FSTUtilTest.java
@@ -1,11 +1,20 @@
 package cn.net.pap.common.datastructure.catalog;
 
+import cn.net.pap.common.datastructure.catalog.dto.FileSegmentDTO;
 import cn.net.pap.common.datastructure.fst.FST;
 import cn.net.pap.common.datastructure.fst.FSTUtil;
 import cn.net.pap.common.datastructure.fst.ValueLocationDTO;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class FSTUtilTest {
 
@@ -31,5 +40,107 @@ public class FSTUtilTest {
 
         List<String> maxMatchList = FSTUtil.maxMatch(text2, dict);
         System.out.println(maxMatchList);
+    }
+
+    /**
+     * Optimized implementation took: 175623 ms
+     * atomicInteger size: 1000000000
+     *
+     * [ValueLocationDTO{text='Kano;27.2', start=3, end=12}]
+     * segment took: 13 ms
+     * @throws Exception
+     */
+    // @Test
+    public void billionSegment() throws Exception {
+        String filePath = "d:\\measurements.txt";
+
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        FST dict = new FST();
+
+        long startTime = System.currentTimeMillis();
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(filePath));
+            FileChannel fileChannel = fileInputStream.getChannel();
+            List<FileSegmentDTO> fileSegmentDTOS = getFileSegments(new File(filePath), fileChannel);
+            fileSegmentDTOS.parallelStream().forEach( fileSegmentDTO -> {
+                try {
+                    MappedByteBuffer buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, fileSegmentDTO.getStart(), fileSegmentDTO.getEnd() - fileSegmentDTO.getStart());
+                    processBuffer(buffer, atomicInteger, dict);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Optimized implementation took: " + (endTime - startTime) + " ms");
+        System.out.println("atomicInteger size: " + (atomicInteger));
+        System.out.println();
+
+        long startTimeSeg = System.currentTimeMillis();
+        List<ValueLocationDTO> result2 = FSTUtil.maxMatchLocation("气象站Kano;27.2是某温度", dict);
+        System.out.println(result2);
+        long endTimeSeg = System.currentTimeMillis();
+        System.out.println("segment took: " + (endTimeSeg - startTimeSeg) + " ms");
+
+    }
+
+    private static List<FileSegmentDTO> getFileSegments(final File file, final FileChannel fileChannel) throws IOException {
+        final int numberOfSegments = Runtime.getRuntime().availableProcessors();
+        final long fileSize = file.length();
+        final long segmentSize = fileSize / numberOfSegments;
+        final List<FileSegmentDTO> segments = new ArrayList<>();
+        if (segmentSize < 1000) {
+            segments.add(new FileSegmentDTO(0, fileSize, fileChannel));
+            return segments;
+        }
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r")) {
+            long segStart = 0;
+            long segEnd = segmentSize;
+            while (segStart < fileSize) {
+                segEnd = findSegment(randomAccessFile, segEnd, fileSize);
+                segments.add(new FileSegmentDTO(segStart, segEnd, fileChannel));
+                segStart = segEnd;
+                segEnd = Math.min(fileSize, segEnd + segmentSize);
+            }
+        }
+        return segments;
+    }
+
+    private static long findSegment(RandomAccessFile raf, long location, final long fileSize) throws IOException {
+        raf.seek(location);
+        while (location < fileSize) {
+            location++;
+            if (raf.read() == '\n')
+                return location;
+        }
+        return location;
+    }
+
+    private static void processBuffer(MappedByteBuffer buffer, AtomicInteger atomicInteger, FST dict) {
+        StringBuilder line = new StringBuilder();
+
+        while (buffer.hasRemaining()) {
+            char c = (char) buffer.get();
+            if (c == '\n' || c == '\r') {
+                if (line.length() > 0) {
+                    dict.addWord(line.toString());
+                    atomicInteger.incrementAndGet();
+                    line.setLength(0);
+                }
+            } else {
+                line.append(c);
+            }
+        }
+
+        if (line.length() > 0) {
+            dict.addWord(line.toString());
+            atomicInteger.incrementAndGet();
+        }
     }
 }
