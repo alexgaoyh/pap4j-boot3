4c3563be8b10b97bb842421fa6474765f49a433d
alexgaoyh <alexgaoyh@sina.com>
2024-08-22 11:12:09 +0800

    add: spring-boot-admin.

---
+++ pap4j-boot3-example/pap4j-boot3-example-actuator-admin/src/main/java/cn/net/pap/example/actuator/admin/ActuatorAdminApplication.java
@@ -0,0 +1,14 @@
+package cn.net.pap.example.actuator.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class ActuatorAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorAdminApplication.class, args);
+    }
+}
