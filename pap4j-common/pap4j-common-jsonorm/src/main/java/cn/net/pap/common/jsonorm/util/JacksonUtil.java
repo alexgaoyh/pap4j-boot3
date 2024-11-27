ef7515ddbc6cdeb5bb116e5038bbd388c4f6ccbe
alexgaoyh <alexgaoyh@sina.com>
2024-11-27 15:32:27 +0800

    fix: jackson any object filter field.

--- pap4j-common/pap4j-common-jsonorm/src/main/java/cn/net/pap/common/jsonorm/util/JacksonUtil.java
+++ pap4j-common/pap4j-common-jsonorm/src/main/java/cn/net/pap/common/jsonorm/util/JacksonUtil.java
@@ -1,12 +1,15 @@
 package cn.net.pap.common.jsonorm.util;
 
 import com.fasterxml.jackson.databind.BeanDescription;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationConfig;
 import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
 import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class JacksonUtil {
@@ -35,6 +38,40 @@ public class JacksonUtil {
             return beanProperties.stream().filter(writer -> !fieldsToExclude.contains(writer.getName()))
                     .collect(Collectors.toList());
         }
+    }
+
+    /**
+     * 清空不在目标结构中的字段
+     *
+     * @param jsonObj         原始对象的JsonNode
+     * @param targetStructure 目标结构的JsonNode
+     * @return 过滤后的JsonNode
+     */
+    public static JsonNode filterJson(JsonNode jsonObj, JsonNode targetStructure) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        com.fasterxml.jackson.databind.node.ObjectNode newJsonNode = objectMapper.createObjectNode();
+
+        // 遍历原始JSON对象的字段
+        Iterator<Map.Entry<String, JsonNode>> fields = jsonObj.fields();
+        while (fields.hasNext()) {
+            Map.Entry<String, JsonNode> entry = fields.next();
+            String fieldName = entry.getKey();
+            JsonNode fieldValue = entry.getValue();
+
+            // 如果目标结构中有该字段，则将其添加到新的JSON节点中
+            if (targetStructure.has(fieldName)) {
+                if (fieldValue.isObject()) {
+                    // 如果是对象类型，则递归处理
+                    newJsonNode.set(fieldName, filterJson(fieldValue, targetStructure.get(fieldName)));
+                } else {
+                    // 如果是普通字段，直接赋值
+                    newJsonNode.set(fieldName, fieldValue);
+                }
+            }
+        }
+
+        return newJsonNode;
     }
 
 }
--- pap4j-common/pap4j-common-jsonorm/src/test/java/cn/net/pap/common/jsonorm/JacksonUtilTest.java
+++ pap4j-common/pap4j-common-jsonorm/src/test/java/cn/net/pap/common/jsonorm/JacksonUtilTest.java
@@ -1,6 +1,7 @@
 package cn.net.pap.common.jsonorm;
 
 import cn.net.pap.common.jsonorm.util.JacksonUtil;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +14,7 @@ public class JacksonUtilTest {
 
     /**
      * 序列化的时候，忽略特定字段
+     *
      * @throws Exception
      */
     @Test
@@ -32,6 +34,27 @@ public class JacksonUtilTest {
         ObjectMapper mapper = JacksonUtil.createObjectMapper(fieldsToExclude);
         String json = mapper.writeValueAsString(jacksonDTO);
         System.out.println(json);
+    }
+
+    /**
+     * 根据前后的字段, 过滤掉不需要的字段
+     * @throws Exception
+     */
+    @Test
+    public void filterJSONTest() throws Exception {
+        String originalJson = "{\"name\":\"name\",\"age\":\"age\",\"ext\":{\"detail\":{\"dName\":\"dName\",\"dAge\":\"dAge\"}}}";
+
+        String targetStructureJson = "{\"name\":null,\"ext\":{\"detail\":{\"dName\":null}}}";
+
+        // 解析原始JSON和目标结构
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode originalNode = objectMapper.readTree(originalJson);
+        JsonNode targetStructureNode = objectMapper.readTree(targetStructureJson);
+
+        // 过滤不需要的字段
+        JsonNode filteredJson = JacksonUtil.filterJson(originalNode, targetStructureNode);
+        // 输出结果
+        System.out.println("Filtered JSON: " + objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(filteredJson));
     }
 
     public class JacksonDTO implements Serializable {
