516bc805c125125e0509d52858d84032e57e873b
alexgaoyh <alexgaoyh@sina.com>
2024-12-02 13:25:47 +0800

    add: Doris UNIQUE KEY .

--- pap4j-boot3-example/pap4j-boot3-example-doris/src/main/resources/schema-mysql.sql
+++ pap4j-boot3-example/pap4j-boot3-example-doris/src/main/resources/schema-mysql.sql
@@ -6,8 +6,10 @@ CREATE TABLE if not exists doris
     doris_name CHAR(20) COMMENT "doris_name",
     doris_remark CHAR(20) COMMENT "doris_name"
 )
+UNIQUE KEY(`id`)
 COMMENT "doris"
 DISTRIBUTED BY HASH(id) BUCKETS 32
 PROPERTIES (
-    "replication_num" = "1"
+   "replication_num" = "1",
+   "enable_unique_key_merge_on_write" = "true"
 );
