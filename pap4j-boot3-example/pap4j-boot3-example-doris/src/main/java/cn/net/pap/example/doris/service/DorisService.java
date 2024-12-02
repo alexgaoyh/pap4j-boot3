6f75a06a3e6e84734b18a809399cf53069f68d1d
alexgaoyh <alexgaoyh@sina.com>
2024-12-02 14:05:52 +0800

    add: doris use insert instead of update.

--- pap4j-boot3-example/pap4j-boot3-example-doris/src/main/java/cn/net/pap/example/doris/service/DorisService.java
+++ pap4j-boot3-example/pap4j-boot3-example-doris/src/main/java/cn/net/pap/example/doris/service/DorisService.java
@@ -23,6 +23,8 @@ public class DorisService {
             stmt.addBatch("begin");
             stmt.addBatch("INSERT INTO doris(id, doris_name, doris_remark) VALUES (11, '1', '1')");
             stmt.addBatch("INSERT INTO doris(id, doris_name, doris_remark) VALUES (22, '2', '2')");
+            // 使用 insert 完成列数据的更新
+            stmt.addBatch("INSERT INTO doris(id, doris_name, doris_remark) VALUES (22, '2-update', '2-update')");
             // 如果过长的话，会统一回退；
             stmt.addBatch("INSERT INTO doris(id, doris_name, doris_remark) VALUES (33, 'largelargelargelarge', null)");
             stmt.executeBatch();
