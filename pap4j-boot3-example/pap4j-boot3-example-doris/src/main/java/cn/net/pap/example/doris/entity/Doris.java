94d5dbd85d62833d7fcdff0f4398fe2f987c4d27
alexgaoyh <alexgaoyh@sina.com>
2024-12-03 13:56:34 +0800

    add: doris json ObjectNode fieldType.

--- pap4j-boot3-example/pap4j-boot3-example-doris/src/main/java/cn/net/pap/example/doris/entity/Doris.java
+++ pap4j-boot3-example/pap4j-boot3-example-doris/src/main/java/cn/net/pap/example/doris/entity/Doris.java
@@ -1,10 +1,13 @@
 package cn.net.pap.example.doris.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.Data;
 
 @Data
-@TableName("doris")
+@TableName(value = "doris", autoResultMap = true)
 public class Doris {
 
     private Long id;
@@ -13,6 +16,7 @@ public class Doris {
 
     private String dorisRemark;
 
-    private Object dorisJson;
+    @TableField(typeHandler = JacksonTypeHandler.class)
+    private ObjectNode dorisJson;
 
 }
