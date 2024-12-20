70cedcfa35d3e851bc5c2780a2f4ee0a70eae321
alexgaoyh <alexgaoyh@sina.com>
2024-12-20 09:04:01 +0800

    add: stream string.

--- pap4j-boot3-example/pap4j-boot3-example-admin/src/main/java/cn/net/pap/example/admin/controller/BeanController.java
+++ pap4j-boot3-example/pap4j-boot3-example-admin/src/main/java/cn/net/pap/example/admin/controller/BeanController.java
@@ -7,11 +7,20 @@ import cn.net.pap.example.bean.config.dto.ExampleBeanDTO;
 import cn.net.pap.example.user.config.dto.ExampleUserDTO;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -108,6 +117,65 @@ public class BeanController {
     @GetMapping("/getArray")
     public String getArray(@RequestParam(value = "arrays") List<String> arrays) throws IOException {
         return arrays.toString();
+    }
+
+    /**
+     * stream string
+     * @param response
+     * @throws IOException
+     */
+    @GetMapping("/stream-strings")
+    public void streamStrings(HttpServletResponse response) throws IOException {
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
+
+        try {
+            for (int i = 0; i < 10; i++) {
+                String content = "Line " + i + " - " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "\n";
+                response.getOutputStream().write(content.getBytes());
+                response.getOutputStream().flush(); // 确保内容被立即发送到客户端
+                Thread.sleep(1000); // 暂停1秒以模拟延迟
+            }
+        } catch (IOException e) {
+            // 处理可能的IO异常
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            // 处理线程中断异常
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    /**
+     * stream string with http request
+     * @param response
+     * @throws IOException
+     */
+    @GetMapping("/stream-strings-api")
+    public void streamStringsAPI(HttpServletResponse response) throws IOException {
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            ClientHttpResponse clientHttpResponse = restTemplate.execute(
+                    "http://localhost:8080/stream-strings",
+                    HttpMethod.GET,
+                    null,
+                    responseExtractor -> {
+                        InputStream inputStream = responseExtractor.getBody();
+                        OutputStream outputStream = response.getOutputStream();
+                        byte[] buffer = new byte[1024];
+                        int bytesRead;
+                        while ((bytesRead = inputStream.read(buffer)) != -1) {
+                            outputStream.write(buffer, 0, bytesRead);
+                            outputStream.flush();
+                        }
+                        return null;
+                    }
+            );
+        } catch (ResourceAccessException e) {
+            // 处理可能的网络异常
+            e.printStackTrace();
+        }
     }
 
 }
