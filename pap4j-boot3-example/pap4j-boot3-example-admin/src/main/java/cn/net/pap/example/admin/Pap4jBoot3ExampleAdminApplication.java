51614c562f90f28ae35610cbb3ad94b7479fc313
alexgaoyh <alexgaoyh@sina.com>
2025-03-13 10:31:37 +0800

    fix: check file md5 after run.

--- pap4j-boot3-example/pap4j-boot3-example-admin/src/main/java/cn/net/pap/example/admin/Pap4jBoot3ExampleAdminApplication.java
+++ pap4j-boot3-example/pap4j-boot3-example-admin/src/main/java/cn/net/pap/example/admin/Pap4jBoot3ExampleAdminApplication.java
@@ -1,7 +1,16 @@
 package cn.net.pap.example.admin;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @SpringBootApplication(scanBasePackages = "cn.net.pap.example")
 public class Pap4jBoot3ExampleAdminApplication {
@@ -10,4 +19,44 @@ public class Pap4jBoot3ExampleAdminApplication {
         SpringApplication.run(Pap4jBoot3ExampleAdminApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner shutdownAfterStartup() {
+        return args -> {
+            // 运行时获取所有类文件.
+            try {
+                PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+                Resource[] resources = resolver.getResources("classpath*:cn/net/pap/**/*.class");
+
+                for (Resource resource : resources) {
+                    try (InputStream inputStream = resource.getInputStream()) {
+                        String md5 = calculateMD5(inputStream);
+                        System.out.println(resource.getURL() + " -> MD5: " + md5);
+                    } catch (Exception e) {
+                        throw new RuntimeException("启动后校验失败,服务关闭：" + "Error processing file: " + resource.getFilename() + " - " + e.getMessage());
+                    }
+                }
+            } catch (Exception e) {
+                throw new RuntimeException("启动后校验失败,服务关闭：" + e);
+            }
+
+        };
+    }
+
+    private static String calculateMD5(InputStream inputStream) throws NoSuchAlgorithmException, IOException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] buffer = new byte[8192];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            md.update(buffer, 0, bytesRead);
+        }
+        return bytesToHex(md.digest());
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
 }
