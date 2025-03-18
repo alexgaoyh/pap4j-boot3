fbc19c2b83ca9e1fb97f339f9d8998dfed716648
alexgaoyh <alexgaoyh@sina.com>
2025-03-18 16:07:23 +0800

    fix: simple pstmt sql logger.

--- pap4j-boot3-example/pap4j-boot3-example-proguard/src/test/java/cn/net/pap/example/proguard/StreamingTest.java
+++ pap4j-boot3-example/pap4j-boot3-example-proguard/src/test/java/cn/net/pap/example/proguard/StreamingTest.java
@@ -13,6 +13,8 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -26,6 +28,8 @@ import java.util.Map;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {cn.net.pap.example.proguard.Pap4jBoot3ExampleProguardApplication.class})
 public class StreamingTest {
+
+    private static final Logger log = LoggerFactory.getLogger(StreamingTest.class);
 
     @Autowired
     private IProguardService proguardService;
@@ -61,6 +65,8 @@ public class StreamingTest {
                         preparedStatement.setInt(1, 9999);
                         preparedStatement.setLong(2, entity.getProguardId());
                         preparedStatement.addBatch();
+
+                        log.info(preparedStatement.toString());
 
                         if (++count % batchSize == 0) {
                             preparedStatement.executeBatch();
