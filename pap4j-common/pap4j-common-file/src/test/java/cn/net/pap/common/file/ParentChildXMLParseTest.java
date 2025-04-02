21a685e52436493419ed0f050e21644aa66ed596
alexgaoyh <alexgaoyh@sina.com>
2025-04-02 13:25:36 +0800

    add: parent child diff node.

--- pap4j-common/pap4j-common-file/src/test/java/cn/net/pap/common/file/ParentChildXMLParseTest.java
+++ pap4j-common/pap4j-common-file/src/test/java/cn/net/pap/common/file/ParentChildXMLParseTest.java
@@ -37,6 +37,24 @@ public class ParentChildXMLParseTest {
 
         Chapter root = parseChapter(doc.getDocumentElement());
         printStructure(root, 0);
+
+
+        Document doc2 = builder.parse("C:\\Users\\86181\\Desktop\\chapter2.xml");
+        /**
+         * <book>
+         *   <chapter title="第一章">
+         *     <section title="第一节">
+         *       <paragraph title="第一小节"/>
+         *     </section>
+         *     <chapter title="第二节"/>
+         *   </chapter>
+         *   <appendix title="附录">
+         *     <note title="注意事项"/>
+         *   </appendix>
+         * </book>
+         */
+        XmlNode root2 = parseXmlNode2(doc2.getDocumentElement());
+        printStructure2(root2, 0);
     }
 
     static class Chapter {
@@ -65,6 +83,44 @@ public class ParentChildXMLParseTest {
         System.out.println(indent + "└─ " + chapter.title);
         for (Chapter sub : chapter.subChapters) {
             printStructure(sub, level + 1);
+        }
+    }
+
+    static class XmlNode {
+        String nodeType;  // 节点类型，如chapter, section, paragraph等
+        String title;
+        List<XmlNode> children = new ArrayList<>();
+    }
+
+    private static XmlNode parseXmlNode2(Element element) {
+        XmlNode node = new XmlNode();
+        node.nodeType = element.getNodeName();
+        node.title = element.hasAttribute("title") ? element.getAttribute("title") : "";
+
+        NodeList children = element.getChildNodes();
+        for (int i = 0; i < children.getLength(); i++) {
+            Node childNode = children.item(i);
+            if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+                XmlNode child = parseXmlNode2((Element) childNode);
+                node.children.add(child);
+            }
+        }
+        return node;
+    }
+
+    private static void printStructure2(XmlNode node, int level) {
+        StringBuilder indent = new StringBuilder();
+        for (int i = 0; i < level; i++) indent.append("  ");
+
+        // 显示节点类型和标题（如果有）
+        String nodeInfo = node.nodeType;
+        if (!node.title.isEmpty()) {
+            nodeInfo += " [title: " + node.title + "]";
+        }
+
+        System.out.println(indent + "└─ " + nodeInfo);
+        for (XmlNode child : node.children) {
+            printStructure2(child, level + 1);
         }
     }
 
