7dfe4f86e53eb1520d18fee3e4cc478fdc9eb264
alexgaoyh <alexgaoyh@sina.com>
2025-04-10 17:05:44 +0800

    add: using log debug enable no print.

--- pap4j-boot3-example/pap4j-boot3-example-proguard/src/main/java/cn/net/pap/example/proguard/config/PapStatementInspector.java
+++ pap4j-boot3-example/pap4j-boot3-example-proguard/src/main/java/cn/net/pap/example/proguard/config/PapStatementInspector.java
@@ -1,15 +1,21 @@
 package cn.net.pap.example.proguard.config;
 
 import org.hibernate.resource.jdbc.spi.StatementInspector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * StatementInspector
  */
 public class PapStatementInspector implements StatementInspector {
 
+    private static final Logger log = LoggerFactory.getLogger(PapStatementInspector.class);
+
     @Override
     public String inspect(String sql) {
-        System.out.println("PapStatementInspector.inspect = " + sql);
+        if(log.isDebugEnabled()) {
+            System.out.println("PapStatementInspector.inspect = " + sql);
+        }
         return sql;
     }
 
--- pap4j-boot3-example/pap4j-boot3-example-proguard/src/main/java/cn/net/pap/example/proguard/listener/TransactionCompletionListener.java
+++ pap4j-boot3-example/pap4j-boot3-example-proguard/src/main/java/cn/net/pap/example/proguard/listener/TransactionCompletionListener.java
@@ -3,22 +3,32 @@ package cn.net.pap.example.proguard.listener;
 import jakarta.persistence.PostPersist;
 import jakarta.persistence.PostUpdate;
 import jakarta.persistence.PostRemove;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TransactionCompletionListener {
 
+    private static final Logger log = LoggerFactory.getLogger(TransactionCompletionListener.class);
+
     @PostPersist
     public void onTransactionCompletionPostPersist(Object entity) {
-        System.out.println("onTransactionCompletionPostPersist : " + entity.toString());
+        if(log.isDebugEnabled()) {
+            System.out.println("onTransactionCompletionPostPersist : " + entity.toString());
+        }
     }
 
     @PostUpdate
     public void onTransactionCompletionPostUpdate(Object entity) {
-        System.out.println("onTransactionCompletionPostUpdate : " + entity.toString());
+        if(log.isDebugEnabled()) {
+            System.out.println("onTransactionCompletionPostUpdate : " + entity.toString());
+        }
     }
 
     @PostRemove
     public void onTransactionCompletionPostRemove(Object entity) {
-        System.out.println("onTransactionCompletionPostRemove : " + entity.toString());
+        if(log.isDebugEnabled()) {
+            System.out.println("onTransactionCompletionPostRemove : " + entity.toString());
+        }
     }
 
 }
