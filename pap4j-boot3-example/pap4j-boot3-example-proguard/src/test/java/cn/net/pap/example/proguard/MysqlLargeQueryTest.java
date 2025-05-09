5561dc737f7e7d1dbf438967a070bd0823fc70ac
alexgaoyh <alexgaoyh@sina.com>
2025-05-09 21:52:01 +0800

    add: re large field compare method.

--- pap4j-boot3-example/pap4j-boot3-example-proguard/src/test/java/cn/net/pap/example/proguard/MysqlLargeQueryTest.java
+++ pap4j-boot3-example/pap4j-boot3-example-proguard/src/test/java/cn/net/pap/example/proguard/MysqlLargeQueryTest.java
@@ -52,36 +52,41 @@ public class MysqlLargeQueryTest {
         String url = "jdbc:mysql://127.0.0.1:3306/test?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8";
 
         try (Connection connection = DriverManager.getConnection(url, "root", "alexgaoyh")) {
-            List<Map<String, Object>> allResultList = new ArrayList<>();
-            for (int idx = 0; idx < 1000; idx++) {
-                String sql = "SELECT FILE_NAME, RECO_RESULT FROM _large_field";
+            testQuery(connection, "SELECT FILE_NAME, RECO_RESULT FROM _large_field", "With RECO_RESULT");
 
-                long start = System.currentTimeMillis();
-
-                try (PreparedStatement statement = connection.prepareStatement(sql)) {
-                    try (ResultSet rs = statement.executeQuery()) {
-                        List<Map<String, Object>> resultList = new ArrayList<>();
-                        ResultSetMetaData metaData = rs.getMetaData();
-                        int columnCount = metaData.getColumnCount();
-                        while (rs.next()) {
-                            Map<String, Object> rowMap = new HashMap<>();
-
-                            for (int i = 1; i <= columnCount; i++) {
-                                String columnName = metaData.getColumnName(i);
-                                Object value = rs.getString(i);
-                                rowMap.put(columnName, value);
-                            }
-                            resultList.add(rowMap);
-                        }
-                        allResultList.addAll(resultList);
-                    }
-                } finally {
-                }
-                long endTime = System.currentTimeMillis();
-                // 可以看这个最后的时间，时间越来越长，越来越长，然后 OutOfMemoryError: Java heap space
-                System.out.println(endTime - start);
-            }
+            testQuery(connection, "SELECT FILE_NAME FROM _large_field", "Without RECO_RESULT");
         }
     }
 
+    private void testQuery(Connection connection, String sql, String testName) throws SQLException {
+        System.out.println("Starting test: " + testName);
+        long totalTime = 0;
+
+        for (int idx = 0; idx < 100; idx++) {
+            long start = System.currentTimeMillis();
+
+            try (PreparedStatement statement = connection.prepareStatement(sql);
+                 ResultSet rs = statement.executeQuery()) {
+                List<Map<String, Object>> resultList = new ArrayList<>();
+                ResultSetMetaData metaData = rs.getMetaData();
+                int columnCount = metaData.getColumnCount();
+                while (rs.next()) {
+                    Map<String, Object> rowMap = new HashMap<>();
+
+                    for (int i = 1; i <= columnCount; i++) {
+                        String columnName = metaData.getColumnName(i);
+                        Object value = rs.getObject(i);
+                        rowMap.put(columnName, value);
+                    }
+                    resultList.add(rowMap);
+                }
+            }
+
+            long duration = System.currentTimeMillis() - start;
+            totalTime += duration;
+            System.out.println("Iteration " + idx + ": " + duration + "ms");
+        }
+
+        System.out.println(testName + " - Average time: " + (totalTime/100) + "ms");
+    }
 }
