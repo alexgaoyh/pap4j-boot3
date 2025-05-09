9c581ab285b02801fc4f68628a00366016322989
alexgaoyh <alexgaoyh@sina.com>
2025-05-09 20:59:51 +0800

    add: jdbc large field.

--- pap4j-boot3-example/pap4j-boot3-example-proguard/src/test/java/cn/net/pap/example/proguard/MysqlLargeQueryTest.java
+++ pap4j-boot3-example/pap4j-boot3-example-proguard/src/test/java/cn/net/pap/example/proguard/MysqlLargeQueryTest.java
@@ -1,5 +1,7 @@
 package cn.net.pap.example.proguard;
 
+import org.junit.jupiter.api.Test;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -30,6 +32,43 @@ public class MysqlLargeQueryTest {
                             for (int i = 1; i <= columnCount; i++) {
                                 String columnName = metaData.getColumnName(i);
                                 Object value = rs.getObject(i);
+                                rowMap.put(columnName, value);
+                            }
+                            resultList.add(rowMap);
+                        }
+                        allResultList.addAll(resultList);
+                    }
+                } finally {
+                }
+                long endTime = System.currentTimeMillis();
+                // 可以看这个最后的时间，时间越来越长，越来越长，然后 OutOfMemoryError: Java heap space
+                System.out.println(endTime - start);
+            }
+        }
+    }
+
+    // @Test
+    public void largeFieldTest() throws SQLException {
+        String url = "jdbc:mysql://127.0.0.1:3306/test?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8";
+
+        try (Connection connection = DriverManager.getConnection(url, "root", "alexgaoyh")) {
+            List<Map<String, Object>> allResultList = new ArrayList<>();
+            for (int idx = 0; idx < 1000; idx++) {
+                String sql = "SELECT FILE_NAME, RECO_RESULT FROM _large_field";
+
+                long start = System.currentTimeMillis();
+
+                try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                    try (ResultSet rs = statement.executeQuery()) {
+                        List<Map<String, Object>> resultList = new ArrayList<>();
+                        ResultSetMetaData metaData = rs.getMetaData();
+                        int columnCount = metaData.getColumnCount();
+                        while (rs.next()) {
+                            Map<String, Object> rowMap = new HashMap<>();
+
+                            for (int i = 1; i <= columnCount; i++) {
+                                String columnName = metaData.getColumnName(i);
+                                Object value = rs.getString(i);
                                 rowMap.put(columnName, value);
                             }
                             resultList.add(rowMap);
