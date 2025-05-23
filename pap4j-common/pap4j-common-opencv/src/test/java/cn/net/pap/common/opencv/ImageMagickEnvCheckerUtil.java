c1a0dd6fb5fd5a653578422538a9bad824efe3f0
alexgaoyh <alexgaoyh@sina.com>
2025-05-23 15:42:41 +0800

    add: ImageMagick command.

--- pap4j-common/pap4j-common-opencv/src/test/java/cn/net/pap/common/opencv/ImageMagickEnvCheckerUtil.java
+++ pap4j-common/pap4j-common-opencv/src/test/java/cn/net/pap/common/opencv/ImageMagickEnvCheckerUtil.java
@@ -54,5 +54,28 @@ public class ImageMagickEnvCheckerUtil {
     public static Boolean getEnvMagickBool() {
         return envMagickBool;
     }
-    
+
+    // ---- 命令汇总
+    // 获取图像DPI  :  magick identify -format "%x - %y" input.jpg
+    // 获取图像像素  :  magick identify -format "%w - %h" input.jpg
+    // 有像素和DPI，就可以算出来图像的物理尺寸
+    //
+    // Canny边缘检测 : magick 20.jpg -canny  0x1+10%+30% edges.png
+    // 临时命令：      magick 20.jpg -deskew 40% -verbose info:
+
+    // 黑框逻辑，不完善. 假设图像的宽高是 597*836 ，  黑色0 -> 白色1 ， 可以使用下面的命令进行分析处理.
+    // 上边缘
+    //     magick input.jpg -crop 100%x1+0+0 +repage top.jpg
+    //     magick top.jpg -colorspace gray -format "%[fx:mean]" info:
+    // 下边缘
+    //     magick input.jpg -crop 100%x1+0+826 +repage bottom.jpg
+    //     magick bottom.jpg -colorspace gray -format "%[fx:mean]" info:
+    // 左边缘
+    //     magick input.jpg -crop 2x100%+0+0 +repage left.jpg
+    //     magick left.jpg -colorspace gray -format "%[fx:mean]" info:
+    // 右边缘
+    //     magick input.jpg -crop 2x100%+581+0 +repage right.jpg
+    //     magick right.jpg -colorspace gray -format "%[fx:mean]" info:
+
+
 }
