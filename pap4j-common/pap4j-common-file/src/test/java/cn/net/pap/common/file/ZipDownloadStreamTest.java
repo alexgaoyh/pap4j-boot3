b16a18ee9989f82f83240f7ad596071dd176be42
alexgaoyh <alexgaoyh@sina.com>
2025-06-19 16:10:04 +0800

    add: Zip Download Stream Test.

---
+++ pap4j-common/pap4j-common-file/src/test/java/cn/net/pap/common/file/ZipDownloadStreamTest.java
@@ -0,0 +1,44 @@
+package cn.net.pap.common.file;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipDownloadStreamTest {
+
+    @Test
+    public void zipDownloadStream() throws IOException {
+        System.out.println("zip download stream");
+    }
+
+//    public void downloadStreamingZip(HttpServletResponse response) throws IOException {
+//        // 设置响应头
+//        response.setContentType("application/zip");
+//        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"streaming-download.zip\"");
+//
+//        try (OutputStream outputStream = response.getOutputStream();
+//             ZipOutputStream zipOut = new ZipOutputStream(outputStream)) {
+//
+//            // 模拟添加多个文件到ZIP
+//            for (int i = 1; i <= 10; i++) {
+//                // 创建ZIP条目
+//                ZipEntry zipEntry = new ZipEntry("file-" + i + ".txt");
+//                zipOut.putNextEntry(zipEntry);
+//
+//                // 写入文件内容
+//                String fileContent = "This is the content of file " + i;
+//                zipOut.write(fileContent.getBytes());
+//
+//                // 关闭当前条目
+//                zipOut.closeEntry();
+//
+//                // 刷新输出流，确保数据发送到客户端
+//                zipOut.flush();
+//            }
+//        }
+//    }
+
+}
