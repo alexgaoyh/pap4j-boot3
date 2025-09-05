0021e35d1123aebd50435d3d21d903e3fa7efc9d
alexgaoyh <alexgaoyh@sina.com>
2025-09-05 10:53:21 +0800

    add: maven-checkstyle-plugin Instantiation Forbidden.

---
+++ pap4j-common/pap4j-common-datastructure/src/main/java/cn/net/pap/common/datastructure/checkstyle/CheckStyleDTO.java
@@ -0,0 +1,19 @@
+package cn.net.pap.common.datastructure.checkstyle;
+
+import java.io.Serializable;
+
+/**
+ * CheckStyleDTO
+ */
+public class CheckStyleDTO implements Serializable {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ pap4j-common/pap4j-common-datastructure/src/main/java/cn/net/pap/common/datastructure/checkstyle/CheckStyleUtil.java
@@ -0,0 +1,14 @@
+package cn.net.pap.common.datastructure.checkstyle;
+
+/**
+ * CheckStyleUtil
+ */
+public class CheckStyleUtil {
+
+    public CheckStyleDTO init() {
+        // 这里配合 maven-checkstyle-plugin 插件，来验证是否允许实例化 new CheckStyleDTO();
+        // new CheckStyleDTO();
+        return null;
+    }
+
+}
