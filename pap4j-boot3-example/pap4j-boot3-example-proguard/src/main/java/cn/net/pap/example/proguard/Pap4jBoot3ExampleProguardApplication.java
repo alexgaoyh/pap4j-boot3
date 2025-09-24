d7078d97bafcb38133741af9741604bbe959537a
alexgaoyh <alexgaoyh@sina.com>
2025-09-24 16:40:39 +0800

    add: addShutdownHook.

--- pap4j-boot3-example/pap4j-boot3-example-proguard/src/main/java/cn/net/pap/example/proguard/Pap4jBoot3ExampleProguardApplication.java
+++ pap4j-boot3-example/pap4j-boot3-example-proguard/src/main/java/cn/net/pap/example/proguard/Pap4jBoot3ExampleProguardApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(scanBasePackages = "cn.net.pap")
@@ -20,9 +21,16 @@ public class Pap4jBoot3ExampleProguardApplication {
     }
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Pap4jBoot3ExampleProguardApplication.class)
+        ConfigurableApplicationContext context = new SpringApplicationBuilder(Pap4jBoot3ExampleProguardApplication.class)
                 .beanNameGenerator(new Pap4jCustomGenerator())
                 .run(args);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            if (context != null && context.isActive()) {
+                int code = SpringApplication.exit(context);
+                System.out.println(String.format("Web应用优雅关闭，退出码: %d", code));
+            }
+        }));
     }
 
 }
