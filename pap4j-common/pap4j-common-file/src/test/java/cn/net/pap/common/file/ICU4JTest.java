205fa7e6d683ab9b3d12c9e1e6d12fbe19b0ae03
alexgaoyh <alexgaoyh@sina.com>
2025-09-28 16:21:37 +0800

    add: icu4j junit.

---
+++ pap4j-common/pap4j-common-file/src/test/java/cn/net/pap/common/file/ICU4JTest.java
@@ -0,0 +1,40 @@
+package cn.net.pap.common.file;
+
+import com.ibm.icu.text.BreakIterator;
+import com.ibm.icu.text.Collator;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+
+public class ICU4JTest {
+
+    @Test
+    public void test1() {
+        // 提供符合不同语言习惯的字符串排序。 - 拼音
+        Collator collator = Collator.getInstance(Locale.CHINA);
+        List<String> words = Arrays.asList("张三", "李四", "王五");
+        words.sort(collator);
+        System.out.println(words);
+
+        words = Arrays.asList("王五", "张三", "李四");
+        // unicode
+        words.sort(Comparator.naturalOrder());
+        System.out.println(words);
+    }
+
+    @Test
+    public void test2() {
+        String text = "👨‍👩‍👧‍👦𠀀Hello";
+        BreakIterator bi = BreakIterator.getCharacterInstance(Locale.CHINA);
+        bi.setText(text);
+
+        int start = bi.first();
+        for (int end = bi.next(); end != BreakIterator.DONE; start = end, end = bi.next()) {
+            System.out.println(text.substring(start, end));
+        }
+    }
+
+}
