8dc0b8bed60c8e105de69c29114201cdafa56438
alexgaoyh <alexgaoyh@sina.com>
2025-10-24 14:31:45 +0800

    add: webhook parse request.

--- pap4j-boot3-example/pap4j-boot3-example-webhook/src/main/java/cn/net/pap/example/controller/WebhookManagementController.java
+++ pap4j-boot3-example/pap4j-boot3-example-webhook/src/main/java/cn/net/pap/example/controller/WebhookManagementController.java
@@ -4,6 +4,8 @@ import cn.net.pap.example.dto.CreateSubscriptionRequestDTO;
 import cn.net.pap.example.entity.WebhookSubscription;
 import cn.net.pap.example.service.IWebhookSenderService;
 import cn.net.pap.example.service.IWebhookSubscriptionService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +15,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/webhook")
@@ -47,6 +53,26 @@ public class WebhookManagementController {
                                             @RequestBody Map<String, Object> testData) {
         webhookSender.triggerEvent(eventType, testData);
         return ResponseEntity.ok("测试事件已触发");
+    }
+
+    @RequestMapping("/test/webhook")
+    public ResponseEntity<String> testWebhook(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Enumeration<String> headerNames = request.getHeaderNames();
+            Map<String, String> headers = new HashMap<>();
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+                headers.put(headerName, request.getHeader(headerName));
+            }
+            System.out.println("Headers: " + headers);
+
+            String body = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+            System.out.println("Body: " + body);
+
+            return ResponseEntity.ok("");
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
     }
 
     /**
