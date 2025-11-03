9ab6c0493be9d3a2653fdf07b3580fca37885bbd
alexgaoyh <alexgaoyh@sina.com>
2025-11-03 14:45:07 +0800

    add: Recursive CTE in triples.

--- pap4j-boot3-example/pap4j-boot3-example-proguard/src/test/java/cn/net/pap/example/proguard/MysqlBatchTest.java
+++ pap4j-boot3-example/pap4j-boot3-example-proguard/src/test/java/cn/net/pap/example/proguard/MysqlBatchTest.java
@@ -2,7 +2,12 @@ package cn.net.pap.example.proguard;
 
 import org.junit.jupiter.api.Test;
 
-import java.sql.*;
+import java.sql.BatchUpdateException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class MysqlBatchTest {
 
@@ -94,6 +99,78 @@ public class MysqlBatchTest {
                 connection.setAutoCommit(true);
             }
         }
+    }
+
+    /**
+     * CREATE TABLE triples (
+     * id INT AUTO_INCREMENT PRIMARY KEY,
+     * subject VARCHAR(50) NOT NULL,
+     * predicate VARCHAR(50) NOT NULL,
+     * object VARCHAR(50) NOT NULL,
+     * INDEX idx_subject(subject),
+     * INDEX idx_object(object),
+     * INDEX idx_predicate(predicate)
+     * );
+     * INSERT INTO triples(subject, predicate, object) VALUES
+     * ('A', 'friend', 'B'),
+     * ('A', 'friend', 'C'),
+     * ('B', 'friend', 'D'),
+     * ('B', 'friend', 'E'),
+     * ('C', 'friend', 'F'),
+     * ('D', 'friend', 'G'),
+     * ('E', 'friend', 'H'),
+     * ('F', 'friend', 'I');
+     *
+     * @throws SQLException
+     */
+    @Test
+    public void cteTest() throws SQLException {
+        String url = "jdbc:mysql://127.0.0.1:3306/cf?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8";
+
+        // 从节点A开头，小于3跳的所有目标节点，这样拿着这些目标节点，去查找关联到的节点，这样关系就出来了。
+        String cteSql = """
+                        WITH RECURSIVE path_cte(subject, object, depth, full_path) AS (
+                        SELECT subject, object, 1 AS depth,
+                            CONCAT(subject, '->', object) AS full_path
+                        FROM triples
+                        WHERE subject = ?
+                
+                        UNION ALL
+                
+                        SELECT p.subject, t.object, p.depth + 1,
+                            CONCAT(p.full_path, '->', t.object) AS full_path
+                        FROM path_cte p
+                        JOIN triples t ON p.object = t.subject
+                        WHERE FIND_IN_SET(t.object, REPLACE(p.full_path,'->',',')) = 0
+                          AND p.depth < 3
+                    )
+                    SELECT * FROM path_cte ORDER BY depth ASC, subject ASC, object ASC LIMIT ?, ?;
+                """;
+
+        try (Connection connection = DriverManager.getConnection(url, "root", "alexgaoyh");
+             PreparedStatement pstmt = connection.prepareStatement(cteSql)) {
+
+            // 设置参数
+            pstmt.setString(1, "A");
+            int page = 1;
+            int pageSize = 10;
+            int offset = (page - 1) * pageSize;
+            pstmt.setInt(2, offset);
+            pstmt.setInt(3, pageSize);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                System.out.println("subject\tobject\tdepth\tfull_path");
+                while (rs.next()) {
+                    String subject = rs.getString("subject");
+                    String object = rs.getString("object");
+                    int depth = rs.getInt("depth");
+                    String full_path = rs.getString("full_path");
+                    System.out.println(subject + "\t" + object + "\t" + depth + "\t" + full_path);
+                }
+            }
+
+        }
+
     }
 
 
