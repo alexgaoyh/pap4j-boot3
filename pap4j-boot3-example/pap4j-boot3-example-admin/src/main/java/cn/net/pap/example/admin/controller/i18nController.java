f31643b677ed7e86e5fdd87728f21a42148f5b6d
alexgaoyh <alexgaoyh@sina.com>
2025-11-25 09:43:55 +0800

    add: i18n .

---
+++ pap4j-boot3-example/pap4j-boot3-example-admin/src/main/java/cn/net/pap/example/admin/controller/i18nController.java
@@ -0,0 +1,23 @@
+package cn.net.pap.example.admin.controller;
+
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Locale;
+
+@RestController
+public class i18nController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
+    }
+
+}
