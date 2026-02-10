48a23532f4e6c883b26eae4268452e4f682a7647
alexgaoyh <alexgaoyh@sina.com>
2026-02-10 10:41:59 +0800

    add: hanlp pinyin tone mark.

---
+++ pap4j-common/pap4j-common-datastructure/src/test/java/cn/net/pap/common/datastructure/hanlp/HanLPTest.java
@@ -0,0 +1,67 @@
+package cn.net.pap.common.datastructure.hanlp;
+
+import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.dictionary.py.Pinyin;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class HanLPTest {
+
+    /**
+     * https://github.com/hankcs/HanLP/tree/1.x?tab=readme-ov-file#17-%E6%8B%BC%E9%9F%B3%E8%BD%AC%E6%8D%A2
+     */
+    @Test
+    public void pinyinTest() {
+        String text = "重载不是重任";
+        List<Pinyin> pinyinList = HanLP.convertToPinyinList(text);
+        System.out.print("原文,");
+        for (char c : text.toCharArray()) {
+            System.out.printf("%c,", c);
+        }
+        System.out.println();
+
+        System.out.print("拼音（数字音调）,");
+        for (Pinyin pinyin : pinyinList) {
+            System.out.printf("%s,", pinyin);
+        }
+        System.out.println();
+
+        System.out.print("拼音（符号音调）,");
+        for (Pinyin pinyin : pinyinList) {
+            System.out.printf("%s,", pinyin.getPinyinWithToneMark());
+        }
+        System.out.println();
+
+        System.out.print("拼音（无音调）,");
+        for (Pinyin pinyin : pinyinList) {
+            System.out.printf("%s,", pinyin.getPinyinWithoutTone());
+        }
+        System.out.println();
+
+        System.out.print("声调,");
+        for (Pinyin pinyin : pinyinList) {
+            System.out.printf("%s,", pinyin.getTone());
+        }
+        System.out.println();
+
+        System.out.print("声母,");
+        for (Pinyin pinyin : pinyinList) {
+            System.out.printf("%s,", pinyin.getShengmu());
+        }
+        System.out.println();
+
+        System.out.print("韵母,");
+        for (Pinyin pinyin : pinyinList) {
+            System.out.printf("%s,", pinyin.getYunmu());
+        }
+        System.out.println();
+
+        System.out.print("输入法头,");
+        for (Pinyin pinyin : pinyinList) {
+            System.out.printf("%s,", pinyin.getHead());
+        }
+        System.out.println();
+    }
+
+}
