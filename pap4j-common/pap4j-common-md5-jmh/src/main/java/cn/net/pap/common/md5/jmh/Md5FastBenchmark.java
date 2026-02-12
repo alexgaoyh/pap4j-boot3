a34a560b2a3a160e8b10c7f4ade5f61c9bfc2ab2
alexgaoyh <alexgaoyh@sina.com>
2026-02-12 09:21:41 +0800

    add: Benchmark thumbnailator lowMemory.

--- pap4j-common/pap4j-common-md5-jmh/src/main/java/cn/net/pap/common/md5/jmh/Md5FastBenchmark.java
+++ pap4j-common/pap4j-common-md5-jmh/src/main/java/cn/net/pap/common/md5/jmh/Md5FastBenchmark.java
@@ -2,6 +2,7 @@ package cn.net.pap.common.md5.jmh;
 
 import cn.net.pap.common.md5.jmh.util.Md5Fast;
 import cn.net.pap.common.md5.jmh.util.Md5Normal;
+import net.coobird.thumbnailator.Thumbnails;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
@@ -13,6 +14,14 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReadParam;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
@@ -25,9 +34,12 @@ public class Md5FastBenchmark {
 
     private String input;
 
+    private File file;
+
     @Setup
     public void setup() {
         input = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        file = new File("d:\\knowledge\\input.jpg");
     }
 
     @Benchmark
@@ -38,6 +50,45 @@ public class Md5FastBenchmark {
     @Benchmark
     public String md5_normal() throws Exception {
         return Md5Normal.md5(input);
+    }
+
+    @Benchmark
+    public BufferedImage thumb_thumbnailator() throws Exception {
+        BufferedImage image = Thumbnails.of(file).size(141, Integer.MAX_VALUE)
+                .keepAspectRatio(true)
+                .outputFormat("jpg")
+                .outputQuality(0.7).asBufferedImage();
+        return image;
+    }
+
+    @Benchmark
+    public BufferedImage thumb_lowMemory() throws Exception {
+        try (ImageInputStream iis = ImageIO.createImageInputStream(file)) {
+            if (iis == null) {
+                return null;
+            }
+
+            Iterator<ImageReader> readers = ImageIO.getImageReaders(iis);
+            if (!readers.hasNext()) {
+                return null;
+            }
+
+            ImageReader reader = readers.next();
+            try {
+                reader.setInput(iis);
+                int actualWidth = reader.getWidth(0);
+                int sampling = Math.max(actualWidth / 141, 1);
+
+                ImageReadParam param = reader.getDefaultReadParam();
+                param.setSourceSubsampling(sampling, sampling, 0, 0);
+
+                return reader.read(0, param);
+            } finally {
+                reader.dispose();
+            }
+        } catch (IOException e) {
+            return null;
+        }
     }
 
 }
