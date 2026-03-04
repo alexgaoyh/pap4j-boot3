74f07f430f0c90500ee3769da6ea49036daa455e
alexgaoyh <alexgaoyh@sina.com>
2026-03-04 11:29:59 +0800

    add: access get tableName.

--- pap4j-common/pap4j-common-jdbc/src/test/java/cn/net/pap/common/jdbc/SimpleTest.java
+++ pap4j-common/pap4j-common-jdbc/src/test/java/cn/net/pap/common/jdbc/SimpleTest.java
@@ -6,6 +6,9 @@ import org.springframework.jdbc.core.DataClassRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleTest {
@@ -27,6 +30,19 @@ public class SimpleTest {
         dataSource.setUrl(url);
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         try {
+            // get tableName
+            List<String> tableNames = jdbcTemplate.execute((java.sql.Connection con) -> {
+                List<String> names = new ArrayList<>();
+                DatabaseMetaData metaData = con.getMetaData();
+                try (ResultSet rs = metaData.getTables(null, null, "%", new String[]{"TABLE"})) {
+                    while (rs.next()) {
+                        names.add(rs.getString("TABLE_NAME"));
+                    }
+                }
+                return names;
+            });
+            tableNames.forEach(System.out::println);
+
             String sql = "SELECT ID as id, 文字 as temp FROM [CZYX]";
             List<TestRecord> testRecordList = jdbcTemplate.query(sql, new DataClassRowMapper<>(TestRecord.class));
             if (testRecordList != null && !testRecordList.isEmpty()) {
