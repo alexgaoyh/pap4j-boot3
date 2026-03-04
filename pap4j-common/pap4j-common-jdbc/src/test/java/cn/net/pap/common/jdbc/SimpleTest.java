f117f32a545a79397705ef5b35ece1239de57eb1
alexgaoyh <alexgaoyh@sina.com>
2026-03-04 10:40:52 +0800

    add: jdbc access read close.

---
+++ pap4j-common/pap4j-common-jdbc/src/test/java/cn/net/pap/common/jdbc/SimpleTest.java
@@ -0,0 +1,41 @@
+package cn.net.pap.common.jdbc;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.DataClassRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.util.List;
+
+public class SimpleTest {
+
+    public record TestRecord(String id, String temp) {
+    }
+
+    @Test
+    @DisplayName("手动初始化并查询数据")
+    void testStandaloneJdbcQuery() throws Exception {
+        System.setProperty("hsqldb.method_class_names", "net.ucanaccess.*");
+        String dbPath = "C:\\Users\\86181\\Desktop\\test.mdb";
+        String url = "jdbc:ucanaccess://" + dbPath + ";sysline=true;memory=false";
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("net.ucanaccess.jdbc.UcanaccessDriver");
+        dataSource.setUrl(url);
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        try {
+            String sql = "SELECT ID as id, 文字 as temp FROM CZYX";
+            List<TestRecord> testRecordList = jdbcTemplate.query(sql, new DataClassRowMapper<>(TestRecord.class));
+            if (testRecordList != null && !testRecordList.isEmpty()) {
+                testRecordList.forEach(System.out::println);
+            }
+        } finally {
+            try {
+                jdbcTemplate.execute("SHUTDOWN");
+                System.out.println("HSQLDB 引擎已显式关闭，文件锁已释放。");
+            } catch (Exception e) {
+            }
+        }
+    }
+
+}
