a40100908d563c7f7c7b8a6ec96bcd469997e180
alexgaoyh <alexgaoyh@sina.com>
2026-04-28 16:06:17 +0800

    add: minio junit optimize.

--- pap4j-common/pap4j-common-minio/src/test/java/cn/net/pap/common/minio/util/MinioUtilTest.java
+++ pap4j-common/pap4j-common-minio/src/test/java/cn/net/pap/common/minio/util/MinioUtilTest.java
@@ -1,6 +1,8 @@
 package cn.net.pap.common.minio.util;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,26 +13,34 @@ import java.nio.file.StandardCopyOption;
 
 public class MinioUtilTest {
 
-    // @Test
+    private static final Logger log = LoggerFactory.getLogger(MinioUtilTest.class);
+
+    @Test
     public void initTest() {
         try {
 
-            File file = new File("C:\\Users\\86181\\Desktop\\upload.jpg");
+            File file = TestResourceUtil.getFile("jpg.jpg");
+            String objectName = "upload.jpg";
+            String targetFileStr = Files.createTempFile("initTest", ".jpg").toAbsolutePath().toString();
             if(file.exists()){
                 // 上传
                 try (FileInputStream fis = new FileInputStream(file);){
-                    MinioUtil.upload(MinioUtil.DEFAULT_BUCKET, "upload.jpg", fis, file.length(), "image/jpeg");
+                    MinioUtil.upload(MinioUtil.DEFAULT_BUCKET, objectName, fis, file.length(), "image/jpeg");
                 }
                 //下载
-                try(InputStream is = MinioUtil.download(MinioUtil.DEFAULT_BUCKET, "upload.jpg");) {
-                    Files.copy(is, Paths.get("C:\\Users\\86181\\Desktop\\download.jpg"), StandardCopyOption.REPLACE_EXISTING);
+                try(InputStream is = MinioUtil.download(MinioUtil.DEFAULT_BUCKET, objectName);) {
+                    Files.copy(is, Paths.get(targetFileStr), StandardCopyOption.REPLACE_EXISTING);
                 }
                 // 删除
-                MinioUtil.delete(MinioUtil.DEFAULT_BUCKET, "upload.jpg");
+                MinioUtil.delete(MinioUtil.DEFAULT_BUCKET, objectName);
             }
 
         } catch (Exception e) {
-            e.printStackTrace();
+            if(e instanceof java.net.ConnectException) {
+                log.warn("{}", e);
+            } else {
+                log.error("{}", e);
+            }
         }
     }
 }
